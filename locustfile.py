5337f3842ee6e941b68961d02dd86079b3e29db1
Deany256 <gdeandude@gmail.com>
2024-04-22 16:23:30 +0100

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,20 +1,49 @@
-import time
 from locust import HttpUser, task, between
+import random
 
 
-class QuickstartUser(HttpUser):
-    wait_time = between(1, 5)
+class WebsiteUser(HttpUser):
+    wait_time = between(5, 15)  # Wait time between tasks
 
     @task
-    def hello_world(self):
-        self.client.get("/hello")
-        self.client.get("/world")
+    def view_product_list(self):
+        self.client.get("/products")
 
-    @task(3)
-    def view_items(self):
-        for item_id in range(10):
-            self.client.get(f"/item?id={item_id}", name="/item")
-            time.sleep(1)
+    @task
+    def view_product_details(self):
+        product_id = random.randint(1, 100)  # Assuming product IDs range from 1 to 100
+        self.client.get(f"/products/{product_id}")
 
-    def on_start(self):
-        self.client.post("/login", json={"username": "foo", "password": "bar"})
+    @task
+    def add_product(self):
+        payload = {"name": "New Product", "quantity": 10, "price": "10.99"}
+        self.client.post("/add_product", data=payload)
+
+    @task
+    def delete_product(self):
+        product_id = random.randint(1, 100)  # Assuming product IDs range from 1 to 100
+        self.client.post(f"/products/{product_id}/delete")
+
+    @task
+    def edit_product(self):
+        product_id = random.randint(1, 100)  # Assuming product IDs range from 1 to 100
+        payload = {"name": "Edited Product", "quantity": 20, "price": "19.99"}
+        self.client.post(f"/edit_product/{product_id}", data=payload)
+
+    @task
+    def signup(self):
+        payload = {"username": "new_user", "password": "password123"}
+        self.client.post("/signup", data=payload)
+
+    @task
+    def login(self):
+        payload = {"username": "existing_user", "password": "password123"}
+        self.client.post("/login", data=payload)
+
+    @task
+    def logout(self):
+        self.client.get("/logout")
+
+    @task
+    def access_protected_route(self):
+        self.client.get("/protected")
